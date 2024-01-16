881135d54ca11fbf875d1c90a06c585865002551
art-abdulwadud <abdulwadudhajike@gmail.com>
2024-01-17 02:56:29 +0300

    allow any user to add a new project

--- src/components/projects/ProjectsIntro.js
+++ src/components/projects/ProjectsIntro.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { useAtom } from 'jotai';
-import { userAtom } from '../layout';
 import { editingProjectAtom, showAddProjectModalAtom } from './Projects';
 
 const ProjectsIntro = () => {
   const [, setModal] = useAtom(showAddProjectModalAtom);
   const [, setEditingProject] = useAtom(editingProjectAtom);
-  const [user] = useAtom(userAtom);
   return (
     <>
       <div className="surface-section px-4 py-5 md:px-6 lg:px-8">
@@ -16,17 +14,15 @@ const ProjectsIntro = () => {
           <div className="mb-3 lg:mb-0">
             <div className="text-3xl font-medium text-900 mb-3">Projects</div>
             <div className="text-600 mr-0 md:mr-3">Here is where you'll find all the posted fundraisers</div>
-            {user.admin ? (
-              <Button
-                label="Add new project"
-                icon="pi pi-plus"
-                className="text-sm bg-pink-500 border-pink-500 mt-3"
-                onClick={() => {
-                  setEditingProject(false);
-                  setModal(true);
-                }}
-              />
-            ) : null}
+            <Button
+              label="Add new project"
+              icon="pi pi-plus"
+              className="text-sm bg-pink-500 border-pink-500 mt-3"
+              onClick={() => {
+                setEditingProject(false);
+                setModal(true);
+              }}
+            />
           </div>
           <span className="p-input-icon-left w-full md:w-auto">
             <i className="pi pi-search" />
