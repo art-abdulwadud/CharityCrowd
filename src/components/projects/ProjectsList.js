561d3b20d69b2d9f82bfa7738e0f58c2f8ee1563
art-abdulwadud <abdulwadudhajike@gmail.com>
2024-01-18 21:55:19 +0300

    feat(filterProjects): display filtered projects

--- src/components/projects/ProjectsList.js
+++ src/components/projects/ProjectsList.js
@@ -1,10 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useAtom } from 'jotai';
 import { sendQuery } from '../globalFuncs';
 import ReactQueryPreloader from '../ReactQueryPreloader';
 import SingleProject from './SingleProject';
+import { searchProjectAtom } from '../projectdetails/Project';
 
 const ProjectsList = () => {
+  const [searchProject] = useAtom(searchProjectAtom);
   const { isLoading, data, error, isError } = useQuery(['fetchProjects'], async () => {
     const myQuery = `
       query GetAllProjects {
@@ -33,9 +36,13 @@ const ProjectsList = () => {
   }
   return (
     <div className="grid p-2">
-      {data && data.getAllProjects ? data.getAllProjects.map((key) => (
+      {data?.getAllProjects && searchProject?.length < 3 ? data.getAllProjects.map((key) => (
         <SingleProject key={key._id} project={{ ...key, id: key._id }} />
-      )) : null}
+      )) : data?.getAllProjects && searchProject?.length > 2
+        ? data.getAllProjects.filter((key) => key.name.includes(searchProject))
+          .map((key) => (
+            <SingleProject key={key._id} project={{ ...key, id: key._id }} />
+          )) : null}
     </div>
   );
 };
